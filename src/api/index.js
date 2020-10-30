c56be4365a5826eee9e2d03bff96c06f8599ea4a
hare <hendrasadewa30@gmail.com>
2020-10-30 11:58:03 +0700

    add js-doc

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,35 @@
 import httpClient from './httpClient';
 import { CATEGORIES } from './constants';
 
+/**
+ * News Item
+ * @typedef {Object} News
+ * @property {string} judul - judul berita
+ * @property {string} link - link berita, dapat digunakan untuk id detail berita
+ * @property {string} poster - poster berita, dalam jpeg
+ * @property {string} tipe - tipe atau kategori berita
+ * @property {string} waktu - waktu dari berita rilis
+ */
+
+/**
+ * Generic News Response
+ * @typedef {Object} GenericNewsResponse
+ * @property {Array<News>} data - list dari news item
+ * @property {string} error - object error message
+ */
+
+/**
+ * Normalized news list
+ * @typedef { Object } NormalizedNewsList
+ * @property {News} headline - first news item with resized image
+ * @property {string} list - list news item
+ */
+
+/**
+ * Get recent news list
+ * @returns {Promise<NormalizedNewsList|GenericNewsResponse>}
+ * Normalized news with headline or generic News with error
+ */
 async function getRecentNews() {
   try {
     const { data, error } = await httpClient.get();
