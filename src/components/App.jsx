259dc193cb74dafafbff88fe0253033db3e96f6c
hare <hendrasadewa30@gmail.com>
2020-10-30 19:03:09 +0700

    create pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import { Router } from '@reach/router';
+import CategoryPage from '../routes/Category';
 
 import DetailsPage from '../routes/Details';
 import HomePage from '../routes/Home';
+import NotFoundPage from '../routes/NotFound';
+import SearchPage from '../routes/SearchPage';
 
 import './App.css';
 
@@ -10,7 +13,10 @@ function App() {
     <div className="app">
       <Router>
         <HomePage path="/" />
+        <CategoryPage path="/kategori/" />
+        <SearchPage path="/pencarian/" />
         <DetailsPage path="/:category/:id/:slug" />
+        <NotFoundPage path="*" />
       </Router>
     </div>
   );
