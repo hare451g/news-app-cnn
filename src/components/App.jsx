b3b9dd78d18a2c40aab6b4e2ab49aa52bb04f62a
hare <hendrasadewa30@gmail.com>
2020-10-30 15:10:33 +0700

    add router to news details

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
+import { Router } from '@reach/router';
+import NewsDetailsFeature from '../features/NewsDetails';
+
 import RecentNewsFeature from '../features/RecentNews';
 
 function App() {
-  return <RecentNewsFeature />;
+  return (
+    <Router>
+      <RecentNewsFeature path="/" />
+      <NewsDetailsFeature path="/details/:link" />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/features/NewsDetails/index.jsx
@@ -0,0 +1,5 @@
+function NewsDetailsFeature({ link }) {
+  return <div>News details feature {link} </div>;
+}
+
+export default NewsDetailsFeature;
