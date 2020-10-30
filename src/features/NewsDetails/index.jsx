167bd793c7480bab3f1efb8484360babb91c88a1
hare <hendrasadewa30@gmail.com>
2020-10-30 17:09:29 +0700

    implement back button to component

--- src/features/NewsDetails/index.jsx
+++ src/features/NewsDetails/index.jsx
@@ -1,5 +1,57 @@
-function NewsDetailsFeature({ link }) {
-  return <div>News details feature {link} </div>;
+import { useEffect, useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+import reducer, { initialState, effects } from './reducer';
+import NewsDetailLayout from './NewsDetailLayout';
+import BackButton from '../../components/BackButton';
+
+function NewsDetailsFeature({ category, id, slug }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { error, isLoading, judul, poster, body } = state;
+
+  useEffect(() => {
+    effects.fetchDetails(dispatch, category, id, slug);
+  }, [category, id, slug]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (isLoading) {
+    return <div>loading . . . </div>;
+  }
+
+  if (body) {
+    return (
+      <div>
+        <BackButton to="/" label="Recent News" />
+        <NewsDetailLayout
+          body={body}
+          category={category}
+          judul={judul}
+          poster={poster}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      News details feature {category}, {id}, {slug}
+    </div>
+  );
 }
+
+NewsDetailsFeature.propTypes = {
+  category: PropTypes.string,
+  id: PropTypes.string,
+  slug: PropTypes.string,
+};
+
+NewsDetailsFeature.defaultProps = {
+  category: null,
+  id: null,
+  slug: null,
+};
 
 export default NewsDetailsFeature;
