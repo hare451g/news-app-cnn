a2801a38a32b8fa81222dcfa227d1df2160ba80c
hare <hendrasadewa30@gmail.com>
2020-10-30 14:44:46 +0700

    implement Recent news Feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,7 @@
-import { useEffect, useState } from 'react';
-import api from '../api';
-import NewsItemList from './NewsItemList';
+import RecentNewsFeature from '../features/RecentNews';
 
 function App() {
-  const [newsItems, setNewsItems] = useState();
-
-  useEffect(() => {
-    const performFetch = async () => {
-      const { data, error } = await api.getRecentNews();
-      if (data) {
-        setNewsItems(data);
-      }
-    };
-    performFetch();
-  }, []);
-
-  if (newsItems) {
-    const { headline, list } = newsItems;
-    return <NewsItemList list={list} />;
-  }
-
-  return (
-    <div className="App">
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
-  );
+  return <RecentNewsFeature />;
 }
 
 export default App;
